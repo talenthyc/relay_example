ed1d2212e7197dd6fd245f0fac8351025ebfa404
Yuchao Han <hanyc1992@gmail.com>
2023-02-26 15:52:14 +0800

    Refetchable Fragments
    对于 refetchable fragments:
    - 在 initial screen load fetch 里，嵌入在整个大 query 里
    - 对于之后 user interaction 需要对这个 fragment refetch 的时候，会只
      refetch 这个 fragment 的内容(relay 会对应的生成一个只含有这个 fragment
      的 query)

--- newsfeed/src/components/ContactsList.tsx
+++ newsfeed/src/components/ContactsList.tsx
@@ -4,14 +4,20 @@ import { useFragment } from "react-relay";
 import type { ContactsListFragment$key } from "./__generated__/ContactsListFragment.graphql";
 import Card from "./Card";
 import ContactRow from "./ContactRow";
+import SearchInput from "./SearchInput";
+import { useRefetchableFragment } from "react-relay";
+
+const { useState, useTransition } = React;
 
 export type Props = {
   viewer: ContactsListFragment$key;
 };
 
 const ContactsListFragment = graphql`
-  fragment ContactsListFragment on Viewer {
-    contacts {
+  fragment ContactsListFragment on Viewer
+  @refetchable(queryName: "ContactsListRefetchQuery")
+  @argumentDefinitions(search: { type: "String", defaultValue: null }) {
+    contacts(search: $search) {
       id
       ...ContactRowFragment
     }
@@ -19,10 +25,23 @@ const ContactsListFragment = graphql`
 `;
 
 export default function ContactsList({ viewer }: Props) {
-  const data = useFragment(ContactsListFragment, viewer);
+  const [data, refetch] = useRefetchableFragment(ContactsListFragment, viewer);
+  const [isPending, startTransition] = useTransition();
+  const [searchString, setSearchString] = useState("");
+  const onSearchStringChanged = (value: string) => {
+    setSearchString(value);
+    startTransition(() => {
+      refetch({ search: value });
+    });
+  };
   return (
     <Card dim={true}>
       <h3>Contacts</h3>
+      <SearchInput
+        value={searchString}
+        onChange={onSearchStringChanged}
+        isPending={isPending}
+      />
       {data.contacts.map((contact) => (
         <ContactRow key={contact.id} contact={contact} />
       ))}
